9548a240d803c68c0961138bfb2ce905cf715e43
emoskedy <nghilam@tamu.edu>
2025-01-26 09:26:25 -0600

    changed header color to blue and removed the login button in the flightinfo page

--- App.tsx
+++ App.tsx
@@ -15,12 +15,25 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#0078D2', // Set the header color to blue
+          },
+          headerTintColor: '#fff', // Set the header text color to white
+          headerTitleStyle: {
+            fontWeight: 'bold', // Optional: Customize the font weight
+          },
+        }}
+      >
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen
           name="Flightinfo"
           component={Flightinfo}
-          options={{ title: 'Flight Information' }}
+          options={{ 
+            title: 'Flight Information', 
+            headerLeft: () => null,
+          }}
         />
         <Stack.Screen name="Recommendation" component={Recommendation} />
         <Stack.Screen name="Map" component={Map} />
